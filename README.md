44b7d89c83fd3875122147072044678f69e57b32
Kurt Ostergaard <yodude@gmail.com>
2023-08-10 12:11:50 -0400

    Got data from Ricardo instead of Tidy Quant, Yahoo or NASDAQ

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # gamma_factor draws heavily from the paper on the acceleration effect 
-# and gamma factor in asset pricing and the tidyverse
+# and gamma factor in asset pricing, and the tidyverse
--- gamma.R
+++ gamma.R
@@ -1,64 +1,37 @@
 # Gamma factor
+rm(list=ls())
 library(tidyverse)
 library(tidyquant)
 library(scales)
 
-# download the exchange listings from 
-# https://www.nasdaq.com/market-activity/stocks/screener?exchange=nyse
-# and change the name
-# NYSE <- tq_exchange("NYSE") doesn't work
-# AMEX <- jsonlite::fromJSON('https://api.nasdaq.com/api/screener/stocks?tableonly=true&limit=25&offset=0&exchange=AMEX&download=true')
-# head(AMEX$data$rows) doesn't work either
+sample <- read_csv("sample data 2.csv", col_names = FALSE)
+colnames(sample) <- c("ticker", "date", "price")
+sample$date <- as.Date(sample$date, format = "%d-%b-%y")
 
-nyse <- read_csv("nyse.csv", col_names = TRUE)
-amex <- read_csv("amex.csv", col_names = TRUE)
-nasdaq <- read_csv("nasdaq.csv", col_names = TRUE)
-
-nyse$Exchange <- "NYSE"
-amex$Exchange <- "AMEX"
-nasdaq$Exchange <- "NASDAQ"
-
-tickers <- bind_rows(nyse, nasdaq, amex)
-
-zip <- tickers |> 
-  
-index_prices <- tq_get(ticker,
-                       get = "stock.prices",
-                       from = "2000-01-01",
-                       to = "2022-12-31"
-)
-
-# Warning message:
-# There was 1 warning in `dplyr::mutate()`.
-# ℹ In argument: `data.. = purrr::map(...)`.
-# Caused by warning:
-#  ! x = '-', get = 'stock.prices': Error in getSymbols.yahoo(Symbols = "-", env = <environment>, verbose = FALSE, : Unable to import “-”.
-#  HTTP error 404.
-#  Removing -.
-
-index_prices |>
+# pretty chart
+sample |>
   ggplot(aes(
     x = date,
-    y = adjusted,
-    color = symbol
+    y = price,
+    color = ticker
   )) +
   geom_line() +
   labs(
     x = NULL,
     y = NULL,
-    color = NULL,
-    title = "Stock prices of DOW index constituents"
+    title = "Stock prices of index constituents"
   ) +
   theme(legend.position = "none")
 
-all_returns <- index_prices |>
-  group_by(symbol) |>
-  mutate(ret = adjusted / lag(adjusted) - 1) |>
-  select(symbol, date, ret) |>
+all_returns <- sample |>
+  group_by(ticker) |>
+  mutate(ret = price / lag(price) - 1) |>
+  select(ticker, date, ret) |>
   drop_na(ret)
 
-all_returns |>
-  group_by(symbol) |>
+#summary stats
+summary_returns <- all_returns |>
+  group_by(ticker) |>
   summarize(across(
     ret,
     list(
@@ -71,53 +44,26 @@ all_returns |>
   )) |>
   print(n = Inf)
 
-#   ticker <- tq_index("SP500")
-
-volume <- index_prices |>
-  group_by(date) |>
-  summarize(volume = sum(volume * close))
-
-volume |>
-  ggplot(aes(x = date, y = volume)) +
-  geom_line() +
-  labs(
-    x = NULL, y = NULL,
-    title = "Aggregate daily trading volume of DOW index constitutens"
-  ) +
-  scale_y_continuous(labels = unit_format(unit = "B", scale = 1e-9))
-
-volume |>
-  ggplot(aes(x = lag(volume), y = volume)) +
-  geom_point() +
-  geom_abline(aes(intercept = 0, slope = 1),
-              linetype = "dashed"
-  ) +
-  labs(
-    x = "Previous day aggregate trading volume",
-    y = "Aggregate trading volume",
-    title = "Persistence in daily trading volume of DOW index constituents"
-  ) + 
-  scale_x_continuous(labels = unit_format(unit = "B", scale = 1e-9)) +
-  scale_y_continuous(labels = unit_format(unit = "B", scale = 1e-9))
-
-index_prices <- index_prices |>
-  group_by(symbol) |>
+# only use tickers with complete data
+sample <- sample |>
+  group_by(ticker) |>
   mutate(n = n()) |>
   ungroup() |>
   filter(n == max(n)) |>
   select(-n)
 
-returns <- index_prices |>
+# monthly returns
+returns <- sample |>
   mutate(month = floor_date(date, "month")) |>
-  group_by(symbol, month) |>
-  summarize(price = last(adjusted), .groups = "drop_last") |>
+  group_by(ticker, month) |>
+  summarize(price = last(price), .groups = "drop_last") |>
   mutate(ret = price / lag(price) - 1) |>
   drop_na(ret) |>
   select(-price)
 
 returns_matrix <- returns |>
   pivot_wider(
-    names_from = symbol,
+    names_from = ticker,
     values_from = ret
   ) |>
   select(-month)
@@ -135,7 +81,7 @@ tibble(
   volatility = as.numeric(sqrt(t(mvp_weights) %*% Sigma %*% mvp_weights))
 )
 
-benchmark_multiple <- 3
+benchmark_multiple <- 30         # only bc bm return ~1%
 mu_bar <- benchmark_multiple * t(mvp_weights) %*% mu
 C <- as.numeric(t(iota) %*% solve(Sigma) %*% iota)
 D <- as.numeric(t(iota) %*% solve(Sigma) %*% mu)
@@ -175,7 +121,7 @@ res |>
   labs(
     x = "Annualized standard deviation",
     y = "Annualized expected return",
-    title = "Efficient frontier for DOW index constituents"
+    title = "Efficient frontier for group constituents"
   ) +
   scale_x_continuous(labels = percent) +
   scale_y_continuous(labels = percent)
